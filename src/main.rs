7cc06a0d1ac2b223d13dcaf2cb811d97b3b29988
tsucchinoko <ccya2211@icloud.com>
2025-10-22 15:29:25 +0900

    feat: 3つの四角形と対角線を描画

--- src/main.rs
+++ src/main.rs
@@ -106,19 +106,15 @@ pub fn hlt() {
 #[no_mangle]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let mut vram = init_vram(efi_system_table).expect("init_vram_failed");
-    for y in 0..vram.height {
-        for x in 0..vram.width {
-            if let Some(pixel) = vram.pixel_at_mut(x, y) {
-                *pixel = 0x00ff00;
-            }
-        }
-    }
-    for y in 0..vram.height / 2 {
-        for x in 0..vram.width / 2 {
-            if let Some(pixel) = vram.pixel_at_mut(x, y) {
-                *pixel = 0xff0000;
-            }
-        }
+    let vw = vram.width;
+    let vh = vram.height;
+    fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("fill_rect_failed");
+    fill_rect(&mut vram, 0xff0000, 32, 32, 32, 32).expect("fill_rect_failed");
+    fill_rect(&mut vram, 0x00ff00, 64, 64, 64, 64).expect("fill_rect_failed");
+    fill_rect(&mut vram, 0x0000ff, 128, 128, 128, 128)
+        .expect("fill_rect_failed");
+    for i in 0..256 {
+        let _ = draw_point(&mut vram, 0x010101 * i as u32, i, i);
     }
     // println!("Hello, world!");
     loop {
@@ -170,7 +166,7 @@ struct VramBufferInfo {
     buf: *mut u8,
     width: i64,
     height: i64,
-    pixel_per_line: i64,
+    pixels_per_line: i64,
 }
 
 impl Bitmap for VramBufferInfo {
@@ -178,7 +174,7 @@ impl Bitmap for VramBufferInfo {
         4
     }
     fn pixels_per_line(&self) -> i64 {
-        self.pixel_per_line
+        self.pixels_per_line
     }
     fn width(&self) -> i64 {
         self.width
@@ -197,6 +193,53 @@ fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
         buf: gp.mode.frame_buffer_base as *mut u8,
         width: gp.mode.info.horizontal_resolution as i64,
         height: gp.mode.info.vertical_resolution as i64,
-        pixel_per_line: gp.mode.info.pixels_per_scan_line as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
     })
+}
+
+/// Safety
+///
+/// (x, y) must be a valid point in the buf.
+unsafe fn unchecked_draw_point<T: Bitmap>(
+    buf: &mut T,
+    color: u32,
+    x: i64,
+    y: i64,
+) {
+    *buf.unchecked_pixel_at_mut(x, y) = color;
+}
+
+fn draw_point<T: Bitmap>(
+    buf: &mut T,
+    color: u32,
+    x: i64,
+    y: i64,
+) -> Result<()> {
+    *(buf.pixel_at_mut(x, y).ok_or("Out of Range")?) = color;
+    Ok(())
+}
+
+fn fill_rect<T: Bitmap>(
+    buf: &mut T,
+    color: u32,
+    px: i64,
+    py: i64,
+    w: i64,
+    h: i64,
+) -> Result<()> {
+    if !buf.is_in_x_range(px)
+        || !buf.is_in_y_range(py)
+        || !buf.is_in_x_range(px + w - 1)
+        || !buf.is_in_y_range(py + h - 1)
+    {
+        return Err("Out of Range");
+    }
+    for y in py..py + h {
+        for x in px..px + w {
+            unsafe {
+                unchecked_draw_point(buf, color, x, y);
+            }
+        }
+    }
+    Ok(())
 }
